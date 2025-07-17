7d44e3f62c2f4aa3495eabd2dd9a83790c14fbbc
Prashant Vasukar <prashantvasukar@gmail.com>
2025-07-17 10:14:25 +0530

    all

--- ec2-automation/terraform/terraform-import.ps1
+++ ec2-automation/terraform/terraform-import.ps1
@@ -1,4 +1,4 @@
-Write-Output "[INFO] Checking and importing existing AWS resources..."
+Write-Output "[INFO] Checking and importing existing AWS resources…"
 
 $region = "ap-south-1"
 $sgName = "dev-ec2-sg"
@@ -14,29 +14,23 @@ terraform init -input=false
 $sgId = aws ec2 describe-security-groups --region $region --filters Name=group-name,Values=$sgName `
     --query "SecurityGroups[0].GroupId" --output text
 
-if ($sgId -ne "None" -and $sgId -ne "")
-{
+if ($sgId -ne "None" -and $sgId -ne "") {
     Write-Output "[OK] Security Group '$sgName' exists with ID: $sgId"
-    Write-Output "[INFO] Importing Security Group into Terraform state..."
-    terraform import -var-files="dev.tfvars" aws_security_group.ec2_sg $sgId
-}
-else 
-{
+    Write-Output "[INFO] Importing Security Group into Terraform state…"
+    terraform import aws_security_group.ec2_sg $sgId
+} else {
     Write-Output "[INFO] Security Group '$sgName' does not exist. Terraform will create it."
 }
 
 # Check if IAM Role exists in AWS
-$role = aws iam get-role --role-name $roleName --region $region 2>$null
+$role = & aws iam get-role --role-name $roleName 2>$null
 
-if ($LASTEXITCODE -eq 0)
-{
+if ($LASTEXITCODE -eq 0) {
     Write-Output "[OK] IAM Role '$roleName' exists"
-    Write-Output "[INFO] Importing IAM Role into Terraform state..."
-    terraform import -var-files="dev.tfvars" aws_iam_role.ec2_s3_role $roleName
-}
-else
-{
+    Write-Output "[INFO] Importing IAM Role into Terraform state…"
+    terraform import aws_iam_role.ec2_s3_role $roleName
+} else {
     Write-Output "[INFO] IAM Role '$roleName' does not exist. Terraform will create it."
 }
 
-Write-Output "[OK] Pre-import complete. You can now run 'terraform plan' and 'terraform apply'."
+Write-Output "[OK] Pre-import complete. You can now run 'terraform plan -var-file=dev.tfvars' and 'terraform apply -var-file=dev.tfvars'."
