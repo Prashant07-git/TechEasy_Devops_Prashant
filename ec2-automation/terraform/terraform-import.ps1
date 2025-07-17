e7f1d9a2a27d81129bececd5bc986d9b075180d9
Prashant Vasukar <prashantvasukar@gmail.com>
2025-07-17 10:08:49 +0530

    all

--- ec2-automation/terraform/terraform-import.ps1
+++ ec2-automation/terraform/terraform-import.ps1
@@ -8,7 +8,7 @@ $roleName = "dev-ec2-s3-role"
 Set-Location ec2-automation/terraform
 
 # Initialize Terraform
-terraform init
+terraform init -input=false
 
 # Check if SG exists in AWS
 $sgId = aws ec2 describe-security-groups --region $region --filters Name=group-name,Values=$sgName `
@@ -18,7 +18,7 @@ if ($sgId -ne "None" -and $sgId -ne "")
 {
     Write-Output "[OK] Security Group '$sgName' exists with ID: $sgId"
     Write-Output "[INFO] Importing Security Group into Terraform state..."
-    terraform import aws_security_group.ec2_sg $sgId
+    terraform import -var-files="dev.tfvars" aws_security_group.ec2_sg $sgId
 }
 else 
 {
@@ -26,13 +26,13 @@ else
 }
 
 # Check if IAM Role exists in AWS
-$role = aws iam get-role --role-name $roleName --region $region -ErrorAction SilentlyContinue
+$role = aws iam get-role --role-name $roleName --region $region 2>$null
 
 if ($LASTEXITCODE -eq 0)
 {
     Write-Output "[OK] IAM Role '$roleName' exists"
     Write-Output "[INFO] Importing IAM Role into Terraform state..."
-    terraform import aws_iam_role.ec2_s3_role $roleName
+    terraform import -var-files="dev.tfvars" aws_iam_role.ec2_s3_role $roleName
 }
 else
 {
